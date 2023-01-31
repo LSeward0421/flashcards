f59250a9c7bfa9c5eca01cdc6ad04b9de5dcfe97
Lauren Seward <lauren.unal@gmail.com>
2023-01-31 14:43:05 -0600

    Feat: Create initial test setup for round class

---
+++ test/Round-test.js
@@ -0,0 +1,32 @@
+const chai = require("chai");
+const expect = chai.expect;
+const Deck = require("../src/Deck");
+const Card = require("../src/Card");
+const Turn = requre("../src/Turn");
+
+describe("Round", () => {
+  let card1;
+  let card2;
+  let card3;
+  let deck;
+  let round;
+
+  beforeEach(() => {
+    card1 = new Card(
+      1,
+      "What is Larry's favorite kind of potato?",
+      ["mashed", "fried", "baked"],
+      "fried"
+    );
+    card2 = new Card(2, "How many cats does Larry have?", [0, 1, 3], 3);
+    card3 = new Card(
+      3,
+      "What city does Larry live in?",
+      ["Dallas", "Denver", "Boston"],
+      "Dallas"
+    );
+
+    deck = new Deck([card1, card2, card3]);
+    round = new Round(deck);
+  });
+});
