b2593f273babed4c5f4d1e50e3b81439a1ee6fd5
Lauren Seward <lauren.unal@gmail.com>
2023-02-02 09:00:02 -0600

    Refactor: clean up syntax

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Game = require("./src/Game");
 
 // This is where your project starts.
 
-console.log('Your project is running...'); 
+console.log("Your project is running...");
 
 const game = new Game();
 game.start();
